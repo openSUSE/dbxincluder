339acef9741eea0aee6ccc48e0a4e04f3847b3ca
Fabian Vogt <fvogt@suse.com>
2016-04-13 15:41:49 +0200

    Fix coverage

--- tests/test_dbxincluder.py
+++ tests/test_dbxincluder.py
@@ -1,6 +1,14 @@
+import os
 import pytest
 
 import dbxincluder
+
+
+def test_main(capsys):
+    """runs __main__.py"""
+    with pytest.raises(SystemExit):
+        path = os.path.dirname(os.path.realpath(__file__)) + "/../src/dbxincluder/__main__.py"
+        exec(compile(open(path).read(), path, "exec"), {}, {"__name__": "__main__"})
 
 
 def test_version(capsys):
