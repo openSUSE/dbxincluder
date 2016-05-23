a67ae2f3e9eff61bcbf62b11ae71ca418acdad7f
Fabian Vogt <fvogt@suse.com>
2016-05-23 10:58:29 +0200

    Minor fixes for packaging

--- doc/conf.py
+++ doc/conf.py
@@ -13,14 +13,15 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import dbxincluder
 import sys
 import os
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../src'))
+
+import dbxincluder
 
 # -- General configuration ------------------------------------------------
 
@@ -30,10 +31,14 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinxjp.themes.basicstrap'
 ]
+
+# Don't require the theme for man build
+if sys.argv[2] != "man":
+    extensions += ['sphinxjp.themes.basicstrap']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = []
